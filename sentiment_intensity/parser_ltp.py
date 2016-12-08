cf367d94dc3111a029a381f825bfd32057732207
Guadaler <zhangx_fh@163.com>
2016-12-08 13:54:59 +0800

    哈工大LTP工具

--- sentiment_intensity/parser_ltp.py
+++ sentiment_intensity/parser_ltp.py
@@ -11,86 +11,97 @@
 # -------------------------------------------
 import sys
 import urllib, urllib2
-import pprint
-
 import xml.etree.ElementTree as Etree
+import time
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 
-def parser():
+# 依存关系类
+class Relation:
+    def __init__(self, mw, cw, relation, pos_mw, pos_cw, id_mw, id_cw):
+        self.mw = mw  # 修饰词
+        self.cw = cw  # 支配词
+        self.relation = relation
+        self.pos_mw = pos_mw
+        self.pos_cw = pos_cw
+        self.id_mw = id_mw
+        self.id_cw = id_cw
+
+
+def parser(text):
     url_get_base = "http://api.ltp-cloud.com/analysis/"
     args = {
         'api_key': 'o9g0E8g50ZtwuZsmfx4A7juNyw0E0M7fic4dgHSK',
-        'text': '我非常喜欢苹果。',
+        'text': text,
         'pattern': 'dp',
         'format': 'xml'
     }
     result = urllib.urlopen(url_get_base, urllib.urlencode(args))  # POST method
-
     content = result.read().strip(" ")
-    print content
 
-    # content_arr = content.split("\n")
-    # content = ""
-    # for c in content_arr:
-    #     temp = c[c.index("<"):c.rindex(">")+1]
-    #     # print "--",temp
-    #     content = content + temp
+    # 结果解析
+    relation_list = []
 
-    # print chardet.detect(content)
-    # print type(content)
-    # print content
+    try:
+        root = Etree.fromstring(content)
 
-    root = Etree.fromstring(content)
+        word = root.findall('doc/para/sent/word')
 
-    sent = root.findall('doc/para/sent/word')
-    for s in sent:
-        # word = s.find("word")
-        # print "[word] ", word.get("cont")
-        print "[word] ", s.get("cont")
+        # 抽取6种依存关系:ADV, ATT, COO, SBV, VOB, CMP
+        relates = ["ADV", "ATT", "COO", "SBV", "VOB", "CMP"]
 
-    print root[1][0][0][0].get("cont")
-    print root[1][0][0][1].get("cont")
-    print root[1][0][0][2].get("cont")
-    print root[1][0][0][3].get("cont")
+        for w in word:
 
+            relate = w.get("relate")
+            if relates.__contains__(relate):
+                cword = word[int(w.get("parent"))]
+                mw = w.get("cont")  # 修饰词
+                cw = cword.get("cont")  # 支配词
+                relation = relate
+                pos_mw = w.get("pos")
+                pos_cw = cword.get("cont")
+                id_mw = w.get("id")
+                id_cw = cword.get("id")
+                relation = Relation(mw, cw, relation, pos_mw, pos_cw, id_mw, id_cw)
+                relation_list.append(relation)
+    except Exception:
+        print '异常'
 
+    return relation_list
 
 
 if __name__ == "__main__":
-    parser()
 
-    a = '<?xml version="1.0" encoding="utf-8" ?>' \
-        '<xml4nlp>' \
-        '<note sent="y" word="y" pos="y" ne="n" parser="y" semparser="n" lstmsemparser="n" wsd="n" srl="n" />' \
-        '<doc><para id="0">' \
-        '<sent id="0" cont="我非常喜欢苹果。">' \
-        '<word id="0" cont="我" pos="r" parent="2" relate="SBV" />' \
-        '<word id="1" cont="非常" pos="d" parent="2" relate="ADV" />' \
-        '<word id="2" cont="喜欢" pos="v" parent="-1" relate="HED" />' \
-        '<word id="3" cont="苹果" pos="n" parent="2" relate="VOB" />' \
-        '<word id="4" cont="。" pos="wp" parent="2" relate="WP" />' \
-        '</sent></para></doc>' \
-        '</xml4nlp>'
+    # 单句测试
+    # text = "我喜欢苹果,讨厌小米"
+    text_0 = "我喜欢苹果,讨厌小米"
+    result = parser(text_0)
+    for r in result:
+        print r.mw, " - ", r.relation, " - ", r.cw
 
-    b = '<?xml version="1.0" encoding="UTF-8"?>' \
-        '<note><to>World</to>' \
-        '<from>Linvo</from>' \
-        '<heading>Hi</heading>' \
-        '<body>Hello World!</body>' \
-        '</note>'
-
-    # root = Etree.fromstring(a)
-    # print len(root)
-    # print root.find('note')
-    # sent = root.findall("note")
-    # for s in sent:
-    #     word = s.find("to").text()
-    #     print word
-
-
-    # root = Etree.parse("/home/zhangxin/work/workplace_python/stock_industry_sentiment_analysis/sentiment_intensity/text.xml")
-    # note = root.find('note')
-    # print note.get("sent")
+    # 耗时测试
+    # time_begin = time.time()
+    # text = "今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族.今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族." \
+    #        "今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族.今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族." \
+    #        "今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族.今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族." \
+    #        "今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族.今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族" \
+    #        "今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族.今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族" \
+    #        "今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族.今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族" \
+    #        "今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族.今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族" \
+    #        "今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族.今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族" \
+    #        "今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族.今天的股票涨势挺好.我喜欢苹果.我讨厌小米和魅族"
+    # text_arr = text.split(".")
+    # result_list = []
+    # count = 1
+    # for t in text_arr:
+    #     print count, " ", t
+    #     count += 1
+    #     result = parser(t)
+    #     result_list.append(result)
+    #
+    # time_end = time.time()
+    # print "[句子个数] ", len(text_arr)
+    # print "[结果] ", len(result_list)
+    # print "[总耗时]", time_end - time_begin
